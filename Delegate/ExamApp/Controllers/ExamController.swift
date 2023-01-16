b4ced9238d9adc518e4517cbcb0287c2a3938d57
Anton Veldanov <aveldanov@linkedin.com>
2023-01-15 20:07:38 -0800

    iOS

--- Delegate/ExamApp/Controllers/ExamController.swift
+++ Delegate/ExamApp/Controllers/ExamController.swift
@@ -3,18 +3,31 @@
 //  MVC-Exam-Questions
 //
 
-
-import Foundation
 import UIKit
 
-class ExamController :UIViewController {
-    
-    @IBOutlet weak var questionView :QuestionView! 
-    
+// Making this VC a delegate
+// Providing look and feel
+// Providing Data for the tableView
+// Responsibility of the UITableViewDataSource and UITableViewDelegate is Delegated to this Controller
+
+class ExamController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        questionView.questionTextLabel.text = "Is Earth round?"
-        
+
+        tableView.backgroundColor = .red
+
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        return cell
     }
 }
--- Delegate/ExamApp/Views/QuestionView.swift
+++ Delegate/ExamApp/Views/QuestionView.swift
@@ -4,7 +4,6 @@
 //
 
 
-import Foundation
 import UIKit
 
 class QuestionView :UIView {
